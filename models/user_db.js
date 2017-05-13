8426aab7a042f6ede33235f789ba5ed1c7090003
Steven Fox <sfrunner1188@gmail.com>
2017-05-13 13:03:19 -0700

    Created user_db model and sync function for server. Currently set to force true. Need users to create cars_db database

---
+++ models/user_db.js
@@ -0,0 +1,18 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User",
+        {
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            userScore: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            swappedStatus: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        });
+    return User;
+}
