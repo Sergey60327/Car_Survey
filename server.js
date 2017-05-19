32a006d92416cde0fe6793ddc9d402ca3133352b
Steven Fox <sfrunner1188@gmail.com>
2017-05-19 16:58:22 -0700

    Removed unecessary npm packages and require code from server.js

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ app.use(session({
 })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Routes
 // =============================================================
